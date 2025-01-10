422deebc292f8f3487dd6b6a80b40126baf5f80e
gunyu1019 <gunyu1019@gmail.com>
2025-01-10 14:18:24 +0000

    [Deploy] Bump to v1.1.1

--- chzzkpy/__init__.py
+++ chzzkpy/__init__.py
@@ -40,7 +40,7 @@ __title__ = "chzzkpy"
 __author__ = "gunyu1019"
 __license__ = "MIT"
 __copyright__ = "Copyright 2024-present gunyu1019"
-__version__ = "1.1.0"  # version_info.to_string()
+__version__ = "1.1.1"  # version_info.to_string()
 
 
 class VersionInfo(NamedTuple):
@@ -58,5 +58,5 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=1, minor=1, micro=0, release_level=None, serial=0
+    major=1, minor=1, micro=1, release_level=None, serial=0
 )
