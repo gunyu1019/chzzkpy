f929992cefced2ff7169a494e2e34e2b933616ec
gunyu1019 <gunyu1019@gmail.com>
2025-02-28 11:04:47 +0000

    [Feat] #59 - Add Message, Donation data class to parse session event.

---
+++ chzzkpy/offical/message.py
@@ -0,0 +1,54 @@
+"""MIT License
+
+Copyright (c) 2024 gunyu1019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import datetime
+
+from pydantic import Field
+from typing import Any, Literal
+
+from .base_model import ChzzkModel
+
+
+class Profile(ChzzkModel):
+    nickname: str
+    badges: list[Any]
+    verified_mark: bool
+
+
+class Donation(ChzzkModel):
+    type: Literal['CHAT', 'VIDEO'] = Field(alias="donationType")
+    channel: str = Field(alias="channelId")
+    donator_id: str = Field(alias="donatorChannelId")
+    donator_name: str = Field(alias="donatorChannelId")
+    
+    pay_amount: int
+    donation_text: str
+
+
+class Message(ChzzkModel):
+    user_id: str = Field(alias="senderChannelId")
+    
+    profile: Profile
+    content: str
+    channel: str = Field(alias="channelId")
+    created_time: datetime.datetime = Field(alias="messageTime")
+    
