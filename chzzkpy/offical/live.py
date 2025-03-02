50221971811457c109e6633605b80ee15e0f89e3
gunyu1019 <gunyu1019@gmail.com>
2025-03-02 14:15:00 +0000

    [Feat] #42 - Implement Live data class.

--- chzzkpy/offical/live.py
+++ chzzkpy/offical/live.py
@@ -20,11 +20,66 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from __future__ import annotations
 
-from pydantic import Field
+import datetime
+
+from typing import TYPE_CHECKING
+from pydantic import Field, PrivateAttr, computed_field
 
 from .base_model import ChzzkModel
-from .category import Category
+
+if TYPE_CHECKING:
+    from typing import Optional, Literal
+    from .category import Category
+    from .channel import Channel
+
+
+class Live(ChzzkModel):
+    live_id: int
+    live_title: str
+    live_image_url: str = Field(alias="liveThumbnailImageUrl")
+    concurrent_user_count: int
+    open_date: datetime.datetime
+    adult: bool
+    tags: list[str]
+
+    _category_id: Optional[str] = PrivateAttr(default=None)
+    _category_name: Optional[str] = PrivateAttr(default=None)
+    _category_type: Optional[Literal["GAME", "SPORT", "ETC"]] = PrivateAttr(default=None)
+    
+    _channel_id: Optional[str] = PrivateAttr(default=None)
+    _channel_name: Optional[str] = PrivateAttr(default=None)
+    _channel_image: Optional[str] = PrivateAttr(default=None)
+
+    def __init__(self, *args, **kwargs):
+        self._category_id = kwargs.pop("liveCategory")
+        self._category_name = kwargs.pop("liveCategoryValue")
+        self._category_type = kwargs.pop("categoryType")
+
+        self._channel_id = kwargs.pop("channelId")
+        self._channel_name = kwargs.pop("channelName")
+        self._channel_image = kwargs.pop("channelImageUrl")
+        
+        super().__init__(*args, **kwargs)
+
+    @computed_field
+    @property
+    def category(self) -> Category:
+        return Category(
+            id=self._category_id,
+            name=self._category_name,
+            type=self._category_type,
+        )
+
+    @computed_field
+    @property
+    def channel(self) -> Channel:
+        return Channel(
+            id=self._channel_id,
+            name=self._channel_name,
+            image=self._channel_image,
+        )
 
 
 class BrodecastSetting(ChzzkModel):
