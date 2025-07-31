6c220a36f5bf3a45aae710aaede7324c856657cd
gunyu1019 <gunyu1019@gmail.com>
2025-07-31 15:01:18 +0000

    [Fix] Missing import dependience in error.py

--- chzzkpy/error.py
+++ chzzkpy/error.py
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import Any, Literal
+from typing import Any, Literal, Optional
 
 
 class ChzzkpyException(Exception):
